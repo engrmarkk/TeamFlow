c1230f61cdfd71c802bc7570c962397d55f33775
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-06 21:27:06 +0100

    Create account.py

---
+++ endpoints/account.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request
+from utils import return_response
+from http_status import HttpStatus
+from status_res import StatusRes
+from flask_jwt_extended import current_user, jwt_required
+from models import current_user_info
+
+
+account = Blueprint('account', __name__)
+
+ACCOUNT_PREFIX = 'account'
+
+
+@account.route(f'/{ACCOUNT_PREFIX}/dashboard', methods=['GET'])
+@jwt_required()
+def dashboard():
+    return return_response(HttpStatus.OK, status=StatusRes.SUCCESS,
+                           message="User Dashboard", user_info=current_user_info(current_user))
