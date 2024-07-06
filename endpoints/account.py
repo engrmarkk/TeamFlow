9111ba70e39f609dd8e13ec9305a6b05b305b2c7
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-06 22:23:02 +0100

    Update account.py

--- endpoints/account.py
+++ endpoints/account.py
@@ -4,6 +4,7 @@ from http_status import HttpStatus
 from status_res import StatusRes
 from flask_jwt_extended import current_user, jwt_required
 from models import current_user_info
+from decorators import email_verified_required
 
 
 account = Blueprint('account', __name__)
@@ -13,6 +14,7 @@ ACCOUNT_PREFIX = 'account'
 
 @account.route(f'/{ACCOUNT_PREFIX}/dashboard', methods=['GET'])
 @jwt_required()
+@email_verified_required
 def dashboard():
     return return_response(HttpStatus.OK, status=StatusRes.SUCCESS,
                            message="User Dashboard", user_info=current_user_info(current_user))
