33969d6a058f1cbea02e4f942d87da5832a2ac96
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-06 22:22:56 +0100

    decorator

---
+++ decorators/__init__.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from flask_jwt_extended import current_user
+from status_res import StatusRes
+from http_status import HttpStatus
+from utils import return_response
+
+
+# decorator for email verified
+def email_verified_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not current_user.email_verified:
+            return return_response(HttpStatus.FORBIDDEN,
+                                   status=StatusRes.FAILED, message="You are yet to verify your email address")
+        return f(*args, **kwargs)
+    return decorated
