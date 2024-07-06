21168779880a9a2713a98695f9dd7825cba9feee
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-06 13:15:46 +0100

    Create notifications.py

---
+++ models/notifications.py
@@ -0,0 +1,23 @@
+from extensions import db
+from sqlalchemy.orm import relationship
+from utils import gen_uuid
+from datetime import datetime
+
+
+class Notifications(db.Model):
+    __tablename__ = 'notifications'
+    id = db.Column(db.String(50), primary_key=True, default=gen_uuid)
+    message = db.Column(db.Text, nullable=False)
+    date_sent = db.Column(db.DateTime, default=datetime.now())
+    recipient_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    read = db.Column(db.Boolean, default=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Notifications %r>' % self.message
