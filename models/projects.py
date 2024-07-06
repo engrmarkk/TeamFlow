72e6156eb405fa63de69e45fa25c5d6dd53bcf17
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-06 13:15:49 +0100

    Create projects.py

---
+++ models/projects.py
@@ -0,0 +1,26 @@
+from extensions import db
+from sqlalchemy.orm import relationship
+from utils import gen_uuid
+from datetime import datetime
+
+
+class Projects(db.Model):
+    __tablename__ = 'projects'
+    id = db.Column(db.String(50), primary_key=True, default=gen_uuid)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now())
+    owner_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    users = relationship('Users', secondary='users_project', back_populates='projects')
+    tasks = relationship('Tasks', backref='project', lazy=True)
+    document = relationship('Documents', backref='project', lazy=True)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Projects %r>' % self.name
