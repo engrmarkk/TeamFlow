dc9c6101dd0fff317042927f065be3ead0fc1eed
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-10 14:34:01 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -10,20 +10,4 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    try:
-        # r = redis.Redis(host='redis', port=6379, db=0)
-        # redis://default:Speedpay@fintech123!@51.89.226.178/0
-        # connect to the redis link above
-        r = redis.Redis(
-            host="default",
-            port=6379,
-            password="Speedpay@fintech123!",
-            db=0
-        )
-        print("Connected to Redis very wel")
-    except Exception as e:
-        print(f"Failed to connect to Redis: {e}")
-
-    #test celery
-    # add_numbers.delay(10, 20)
     app.run(debug=True, host='0.0.0.0', port=7000)
