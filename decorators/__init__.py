c4b0b887f82e540bfb418bbf60b11943029b1042
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-18 21:46:39 +0100

    super admin required decorator

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -14,3 +14,14 @@ def email_verified_required(f):
                                    status=StatusRes.FAILED, message="You are yet to verify your email address")
         return f(*args, **kwargs)
     return decorated
+
+
+# super admin required
+def super_admin_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not current_user.is_super_admin:
+            return return_response(HttpStatus.FORBIDDEN,
+                                   status=StatusRes.FAILED, message="You are yet to verify your email address")
+        return f(*args, **kwargs)
+    return decorated
