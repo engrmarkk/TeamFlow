a5309b47a9a23ce9aa5b363c54aceada2c9d0550
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-21 13:58:27 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,6 @@ from .users import (Users, authenticate, email_exist,
                     create_otp, get_user_by_email, create_reset_p,
                     get_user_by_reset_p, update_password, current_user_info,
                     get_all_users, get_users_by_organization)
-from .user_project import user_project, get_user_ids_by_project_id
 from .projects import Projects, create_project, get_one_project, get_projects
 from .messages import Messages
 from .notifications import Notifications
