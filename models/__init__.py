2387d9d6709373566820ae19d274315fdc829dcb
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-22 21:58:26 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,14 @@
-from .tasks import Tasks, create_task, get_task_for_project, get_task
+from .tasks import (Tasks, create_task, get_task_for_project,
+                    get_task, task_assigned_to_user,
+                    update_task)
 from .users import (Users, authenticate, email_exist,
                     username_exist, create_user,
                     create_otp, get_user_by_email, create_reset_p,
                     get_user_by_reset_p, update_password, current_user_info,
-                    get_all_users, get_users_by_organization, get_user_by_id)
-from .projects import Projects, create_project, get_one_project, get_projects
+                    get_all_users, get_users_by_organization, get_user_by_id,
+                    update_user_role)
+from .projects import (Projects, create_project,
+                       get_one_project, get_projects, update_project)
 from .messages import Messages
 from .notifications import Notifications
 from .documents import Documents
