d5e114574457b1e70da809e09eaf14471f5bdf9d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-27 16:34:13 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@ from .tasks import (Tasks, create_task, get_task_for_project,
                     get_task, task_assigned_to_user, get_one_task,
                     update_task, get_users_tasks_for_project)
 from .users import (Users, authenticate, email_exist,
-                    username_exist, create_user,
+                    username_exist, create_user, change_password,
                     create_otp, get_user_by_email, create_reset_p,
                     get_user_by_reset_p, update_password, current_user_info,
                     get_all_users, get_users_by_organization, get_user_by_id,
