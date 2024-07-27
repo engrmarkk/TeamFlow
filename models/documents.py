3593bd0696a3b626107dbfe47a1d15b232eaaa08
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-27 13:35:22 +0100

    Update documents.py

--- models/documents.py
+++ models/documents.py
@@ -8,9 +8,21 @@ class Documents(db.Model):
     id = db.Column(db.String(50), primary_key=True, default=gen_uuid)
     name = db.Column(db.String(255), nullable=False)
     url = db.Column(db.String(255), nullable=False)
+    public_id = db.Column(db.String(255)) # This is cloudinary public id
     project_id = db.Column(db.String(50), db.ForeignKey('projects.id'), nullable=False)
     uploaded_by = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
     uploaded_date = db.Column(db.DateTime, default=datetime.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'url': self.url,
+            'public_id': self.public_id,
+            'project_id': self.project_id,
+            'uploaded_by': f"{self.user.last_name} {self.user.first_name}",
+            'uploaded_date': self.uploaded_date.strftime("%d-b-%Y, %H:%M:%S"),
+        }
 
     def save(self):
         db.session.add(self)
@@ -24,7 +36,8 @@ class Documents(db.Model):
 
 
 # create document
-def create_document(name, url, project_id, uploaded_by):
-    document = Documents(name=name, url=url, project_id=project_id, uploaded_by=uploaded_by)
+def create_document(name, url, project_id, uploaded_by, public_id):
+    document = Documents(name=name, url=url, project_id=project_id,
+                         uploaded_by=uploaded_by, public_id=public_id)
     document.save()
     return document
