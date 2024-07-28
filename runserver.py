480b3714e6feb3f22e9c4bee4a1bac2cfc6b2258
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-07-28 01:38:16 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,5 +1,6 @@
 from app_config import create_app
 from dotenv import load_dotenv
+from message_socket import socketio
 import redis
 # from task import add_numbers
 
@@ -8,7 +9,7 @@ load_dotenv()
 
 
 app = create_app()
-socketio = app.extensions['socketio']
+# socketio = app.extensions['socketio']
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=7000)
