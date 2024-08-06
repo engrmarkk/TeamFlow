666f8558079398afb36b1df3fb883340d1d28e3d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-06 21:31:08 +0100

    Create __init__.py

---
+++ celery_config/schedule_config/__init__.py
@@ -0,0 +1,17 @@
+from celery.schedules import crontab
+
+
+CELERY_IMPORTS = ('celery_config.cron_job.jobs',)
+CELERY_TASK_RESULT_EXPIRES = 30
+CELERY_TIMEZONE = 'UTC'
+
+CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERYBEAT_SCHEDULE = {
+    'check_task': {
+        'task': 'celery_config.cron_job.jobs.check_pending_tasks',
+        'schedule': crontab(minute=28, hour=20),
+    },
+}
