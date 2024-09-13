8c438032fbe4a1c2724472198b28a8c44dacdc96
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-09-13 05:09:14 +0100

    Update documents.py

--- models/documents.py
+++ models/documents.py
@@ -8,20 +8,20 @@ class Documents(db.Model):
     id = db.Column(db.String(50), primary_key=True, default=gen_uuid)
     name = db.Column(db.String(255), nullable=False)
     url = db.Column(db.String(255), nullable=False)
-    public_id = db.Column(db.String(255)) # This is cloudinary public id
-    project_id = db.Column(db.String(50), db.ForeignKey('projects.id'), nullable=False)
-    uploaded_by = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    public_id = db.Column(db.String(255))  # This is cloudinary public id
+    project_id = db.Column(db.String(50), db.ForeignKey("projects.id"), nullable=False)
+    uploaded_by = db.Column(db.String(50), db.ForeignKey("users.id"), nullable=False)
     uploaded_date = db.Column(db.DateTime, default=datetime.now())
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'name': self.name,
-            'url': self.url,
-            'public_id': self.public_id,
-            'project_id': self.project_id,
-            'uploaded_by': f"{self.user.last_name} {self.user.first_name}",
-            'uploaded_date': self.uploaded_date.strftime("%d-b-%Y, %H:%M:%S"),
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+            "public_id": self.public_id,
+            "project_id": self.project_id,
+            "uploaded_by": f"{self.user.last_name} {self.user.first_name}",
+            "uploaded_date": self.uploaded_date.strftime("%d-b-%Y, %H:%M:%S"),
         }
 
     def save(self):
@@ -32,12 +32,17 @@ class Documents(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return '<Documents %r>' % self.name
+        return "<Documents %r>" % self.name
 
 
 # create document
 def create_document(name, url, project_id, uploaded_by, public_id):
-    document = Documents(name=name, url=url, project_id=project_id,
-                         uploaded_by=uploaded_by, public_id=public_id)
+    document = Documents(
+        name=name,
+        url=url,
+        project_id=project_id,
+        uploaded_by=uploaded_by,
+        public_id=public_id,
+    )
     document.save()
     return document
