70699bce44690521679f22fc897d5627490c7bae
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-09-13 05:09:13 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,42 @@
-from .tasks import (Tasks, create_task, get_task_for_project,
-                    get_task, task_assigned_to_user, get_one_task,
-                    update_task, get_users_tasks_for_project, statistics, get_tasks_for_user)
-from .users import (Users, authenticate, email_exist,
-                    username_exist, create_user, change_password,
-                    create_otp, get_user_by_email, create_reset_p,
-                    get_user_by_reset_p, update_password, current_user_info,
-                    get_all_users, get_users_by_organization, get_user_by_id,
-                    update_user_role, UserSession)
-from .projects import (Projects, create_project,
-                       get_one_project, get_projects, update_project, is_project_valid)
+from .tasks import (
+    Tasks,
+    create_task,
+    get_task_for_project,
+    get_task,
+    task_assigned_to_user,
+    get_one_task,
+    update_task,
+    get_users_tasks_for_project,
+    statistics,
+    get_tasks_for_user,
+)
+from .users import (
+    Users,
+    authenticate,
+    email_exist,
+    username_exist,
+    create_user,
+    change_password,
+    create_otp,
+    get_user_by_email,
+    create_reset_p,
+    get_user_by_reset_p,
+    update_password,
+    current_user_info,
+    get_all_users,
+    get_users_by_organization,
+    get_user_by_id,
+    update_user_role,
+    UserSession,
+)
+from .projects import (
+    Projects,
+    create_project,
+    get_one_project,
+    get_projects,
+    update_project,
+    is_project_valid,
+)
 from .messages import Messages, create_message, get_messages
 from .notifications import Notifications
 from .documents import Documents, create_document
