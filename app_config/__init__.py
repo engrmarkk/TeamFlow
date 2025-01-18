68da8c3e4dd2355d6fe013fe632788f720ab8a91
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-01-18 09:33:53 +0100

    Update projects.py

--- app_config/__init__.py
+++ app_config/__init__.py
@@ -1,16 +1,26 @@
 from config import config_obj
-from endpoints import (auth as authentication_blueprint, account as account_blueprint,
-                       cloudnary as cloudnary_blueprint)
+from endpoints import (
+    auth as authentication_blueprint,
+    account as account_blueprint,
+    cloudnary as cloudnary_blueprint,
+)
 from flask import Flask
 from extensions import db, migrate, jwt, cors, socketio
 from http_status import HttpStatus
 from utils import return_response
 from status_res import StatusRes
-from models import (Users, Projects, Messages, Notifications, Documents, Tasks,
-                    Organizations)
+from models import (
+    Users,
+    Projects,
+    Messages,
+    Notifications,
+    Documents,
+    Tasks,
+    Organizations,
+)
 
 
-def create_app(config_name='development'):
+def create_app(config_name="development"):
     app = Flask(__name__)
     app.config.from_object(config_obj[config_name])
     db.init_app(app)
@@ -22,19 +32,26 @@ def create_app(config_name='development'):
     # handle 404
     @app.errorhandler(404)
     def not_found(error):
-        return return_response(HttpStatus.NOT_FOUND, status=StatusRes.FAILED,
-                               message="Endpoint Not Found")
+        return return_response(
+            HttpStatus.NOT_FOUND, status=StatusRes.FAILED, message="Endpoint Not Found"
+        )
 
     # handle 500
     @app.errorhandler(500)
     def server_error(error):
-        return return_response(HttpStatus.INTERNAL_SERVER_ERROR, status=StatusRes.FAILED,
-                               message="Network Error")
+        return return_response(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            status=StatusRes.FAILED,
+            message="Network Error",
+        )
 
     @app.errorhandler(405)
     def method_not_allowed(error):
-        return return_response(HttpStatus.METHOD_NOT_ALLOWED, status=StatusRes.FAILED,
-                               message="Method Not Allowed")
+        return return_response(
+            HttpStatus.METHOD_NOT_ALLOWED,
+            status=StatusRes.FAILED,
+            message="Method Not Allowed",
+        )
 
     # function tha returns the current user
     @jwt.user_identity_loader
@@ -52,23 +69,23 @@ def create_app(config_name='development'):
     @app.shell_context_processor
     def shell_context():
         return {
-            'db': db,
-            'Users': Users,
-            'Projects': Projects,
-            'Messages': Messages,
-            'Notifications': Notifications,
-            'Documents': Documents,
-            'Tasks': Tasks,
-            'Organizations': Organizations
+            "db": db,
+            "Users": Users,
+            "Projects": Projects,
+            "Messages": Messages,
+            "Notifications": Notifications,
+            "Documents": Documents,
+            "Tasks": Tasks,
+            "Organizations": Organizations,
         }
 
     # with app.app_context():
     #     db.create_all()
 
     # register blueprints
-    app.register_blueprint(authentication_blueprint, url_prefix='/api/v1')
-    app.register_blueprint(account_blueprint, url_prefix='/api/v1')
-    app.register_blueprint(cloudnary_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(authentication_blueprint, url_prefix="/api/v1")
+    app.register_blueprint(account_blueprint, url_prefix="/api/v1")
+    app.register_blueprint(cloudnary_blueprint, url_prefix="/api/v1")
 
     # load
     return app
