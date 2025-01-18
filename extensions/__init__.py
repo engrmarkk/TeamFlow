fc91289d2f051e0a7abe178f1b5e5b9d9ec83f4c
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-01-18 09:34:20 +0100

    Update __init__.py

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -6,16 +6,16 @@ from sqlalchemy import MetaData
 from flask_socketio import SocketIO
 
 naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
+    "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(column_0_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 jwt = JWTManager()
-socketio = SocketIO(cors_allowed_origins="*", async_mode='threading')
+socketio = SocketIO(cors_allowed_origins="*", async_mode="threading")
 
 cors = CORS()
