e2298b5df088b738e8d5149c882a99ae05a65830
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-01-18 09:33:59 +0100

    Update __init__.py

--- celery_config/schedule_config/__init__.py
+++ celery_config/schedule_config/__init__.py
@@ -1,21 +1,21 @@
 from celery.schedules import crontab
 
 
-CELERY_IMPORTS = ('celery_config.cron_job.jobs',)
+CELERY_IMPORTS = ("celery_config.cron_job.jobs",)
 CELERY_TASK_RESULT_EXPIRES = 30
-CELERY_TIMEZONE = 'UTC'
+CELERY_TIMEZONE = "UTC"
 
-CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ["json", "msgpack", "yaml"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 
 CELERYBEAT_SCHEDULE = {
-    'check_task': {
-        'task': 'celery_config.cron_job.jobs.check_pending_tasks',
-        'schedule': crontab(minute=28, hour=20),
+    "check_task": {
+        "task": "celery_config.cron_job.jobs.check_pending_tasks",
+        "schedule": crontab(minute=28, hour=20),
     },
-    'update_expired_task': {
-        'task': 'celery_config.cron_job.jobs.update_expired_tasks',
-        'schedule': crontab(minute=30, hour=00),
+    "update_expired_task": {
+        "task": "celery_config.cron_job.jobs.update_expired_tasks",
+        "schedule": crontab(minute=30, hour=00),
     },
 }
