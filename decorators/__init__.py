c9ed9f5b86730a0b4605d33581234fd837a36ed9
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-01-18 09:34:10 +0100

    Update __init__.py

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -10,9 +10,13 @@ def email_verified_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         if not current_user.email_verified:
-            return return_response(HttpStatus.FORBIDDEN,
-                                   status=StatusRes.FAILED, message="You are yet to verify your email address")
+            return return_response(
+                HttpStatus.FORBIDDEN,
+                status=StatusRes.FAILED,
+                message="You are yet to verify your email address",
+            )
         return f(*args, **kwargs)
+
     return decorated
 
 
@@ -21,8 +25,11 @@ def super_admin_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         if not current_user.is_super_admin:
-            return return_response(HttpStatus.FORBIDDEN,
-                                   status=StatusRes.FAILED,
-                                   message="You are not authorized to perform this action")
+            return return_response(
+                HttpStatus.FORBIDDEN,
+                status=StatusRes.FAILED,
+                message="You are not authorized to perform this action",
+            )
         return f(*args, **kwargs)
+
     return decorated
