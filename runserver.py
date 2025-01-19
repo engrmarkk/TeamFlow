fa905533c17356c90921e26c8066360bab33dba3
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-01-19 09:35:03 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -9,5 +9,5 @@ load_dotenv()
 
 app = create_app()
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=7000)
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host="0.0.0.0", port=7000)
