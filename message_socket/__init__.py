2fd525b5990826b42f19c032fa17340cb0086ab6
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-01-19 09:34:34 +0100

    Update __init__.py

--- message_socket/__init__.py
+++ message_socket/__init__.py
@@ -6,23 +6,26 @@ from extensions import socketio
 from flask_jwt_extended import current_user, jwt_required
 
 
-@socketio.on('connect')
+@socketio.on("connect")
 def test_connect_handler():
-    print('Client connected')
+    print("Client connected")
 
 
-@socketio.on('join-room')
+@socketio.on("join-room")
 @jwt_required()
 def on_join(data):
-    project_id = data.get('project_id')
+    project_id = data.get("project_id")
     print(current_user.id, "current_user.id")
     if not is_project_valid(project_id):
         print("Invalid project ID")
-        emit('error-message', {
-            'status': HttpStatus.BAD_REQUEST,
-            'status_res': StatusRes.FAILED,
-            'message': "Invalid project ID"
-        })
+        emit(
+            "error-message",
+            {
+                "status": HttpStatus.BAD_REQUEST,
+                "status_res": StatusRes.FAILED,
+                "message": "Invalid project ID",
+            },
+        )
         return
     join_room(project_id)
 
@@ -31,38 +34,47 @@ def on_join(data):
 @socketio.on_error()
 def error_handler(e):
     print(e, "error@socketio.on_error")
-    emit('error-message', {
-        'status': HttpStatus.INTERNAL_SERVER_ERROR,
-        'status_res': StatusRes.FAILED,
-        'message': str(e)
-    })
+    emit(
+        "error-message",
+        {
+            "status": HttpStatus.INTERNAL_SERVER_ERROR,
+            "status_res": StatusRes.FAILED,
+            "message": str(e),
+        },
+    )
 
 
-@socketio.on('send-message')
+@socketio.on("send-message")
 @jwt_required()
 def send_message(data):
-    project_id = data.get('project_id')
+    project_id = data.get("project_id")
     print(project_id, "project_id")
 
     if not is_project_valid(project_id):
         print("Invalid project ID")
-        emit('error-message', {
-            'status': HttpStatus.BAD_REQUEST,
-            'status_res': StatusRes.FAILED,
-            'message': "Invalid project ID"
-        })  # Send error to the requesting client only
+        emit(
+            "error-message",
+            {
+                "status": HttpStatus.BAD_REQUEST,
+                "status_res": StatusRes.FAILED,
+                "message": "Invalid project ID",
+            },
+        )  # Send error to the requesting client only
         return
 
     try:
-        content = data.get('content', None)
+        content = data.get("content", None)
         author_id = current_user.id
 
         if not content:
-            emit('error-message', {
-                'status': HttpStatus.BAD_REQUEST,
-                'status_res': StatusRes.FAILED,
-                'message': "Content is required"
-            })
+            emit(
+                "error-message",
+                {
+                    "status": HttpStatus.BAD_REQUEST,
+                    "status_res": StatusRes.FAILED,
+                    "message": "Content is required",
+                },
+            )
             return
 
         # Assuming create_message is a function that saves the message to the database
@@ -70,12 +82,16 @@ def send_message(data):
         if not msg:
             raise Exception("Network Error")
 
-        emit('receive-message', msg.to_dict(), room=project_id)
+        emit("receive-message", msg.to_dict(), room=project_id)
     except Exception as e:
         # General error handling
         print(e, "error@message_socket/send-message")
-        emit('error', {
-            'status': HttpStatus.INTERNAL_SERVER_ERROR,
-            'status_res': StatusRes.FAILED,
-            'message': "Network Error"
-        }, room=project_id)
+        emit(
+            "error",
+            {
+                "status": HttpStatus.INTERNAL_SERVER_ERROR,
+                "status_res": StatusRes.FAILED,
+                "message": "Network Error",
+            },
+            room=project_id,
+        )
